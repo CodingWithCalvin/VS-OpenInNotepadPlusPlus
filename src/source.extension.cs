19075c9a156eec9e8e3a4b5df7619b50912cbbae
Calvin A. Allen <calvin.allen@outlook.com>
2019-03-11 17:07:48 -0400

    Increment version number

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -11,7 +11,7 @@ namespace OpenInNotepadPlusPlus
 								public const string Name = "Open in Notepad++";
 								public const string Description = @"Adds a menu command that lets you open any solution, project, folder and file in Notepad++";
 								public const string Language = "en-US";
-								public const string Version = "2.0.0";
+								public const string Version = "3.0.0";
 								public const string Author = "Calvin A. Allen";
 								public const string Tags = "notepad++, text, code";
 				}
