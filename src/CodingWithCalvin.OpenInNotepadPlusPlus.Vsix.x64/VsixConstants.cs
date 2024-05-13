a956fa83be99e10d09f7e91ef5984d16af6d4f80
Calvin A. Allen <calvin.allen@outlook.com>
2024-05-13 12:06:14 -0400

    Rearrange for new push

--- src/CodingWithCalvin.OpenInNotepadPlusPlus.Vsix.x64/VsixConstants.cs
+++ src/CodingWithCalvin.OpenInNotepadPlusPlus.Vsix.x64/VsixConstants.cs
@@ -2,12 +2,12 @@
 {
 	internal sealed partial class VsixConstants
 	{
-        public const string Id = "C7A8F218-C175-44F5-97A0-8C2C88FBA176";
+        public const string Id = "VS-OpenInNotepadPlusPlus";
         public const string Name = "Open in Notepad++";
         public const string Description = @"Adds a menu command that lets you open any solution, project, folder and file in Notepad++";
         public const string Language = "en-US";
         public const string Version = "4.1.0";
-        public const string Author = "Calvin A. Allen";
+        public const string Author = "Coding With Calvin";
         public const string Tags = "notepad++, text, code";
     }
 }
